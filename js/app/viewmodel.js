abe649ce3418df59c0ffc2bd9e4264504eb30b5e
Yuki <icykoneko@users.noreply.github.com>
2018-07-21 21:50:50 -0400

    Added fish for patch 4.3

--- js/app/viewmodel.js
+++ js/app/viewmodel.js
@@ -163,7 +163,7 @@ class ViewModel {
       completion: 'all',
       patch: [2, 2.1, 2.2, 2.3, 2.4, 2.5,
               3, 3.1, 3.2, 3.3, 3.4, 3.5,
-              4, 4.1, 4.2],
+              4, 4.1, 4.2, 4.3],
     };
     this.completionManager = new CompletionManager;
     this.theFish = Fishes;
