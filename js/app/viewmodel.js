1699d16fbc21d92fa029a661042266b6ce1b0657
Yuki <icykoneko@users.noreply.github.com>
2019-02-15 22:35:25 -0500

    Added new sorting method, Overall Rarity. Displays any fish up more than 15 minutes from now ordered by rarity rather than next window.

--- js/app/viewmodel.js
+++ js/app/viewmodel.js
@@ -68,14 +68,17 @@ class CompletionManager {
   }
 }
 
-sortByWindowPeriods = function() {
+sorters = function() {
   function shouldLog(a, b) {
-    // fishes = _([a,b]).map((x) => x.name());
-    // return _(fishes).contains("Ghost Carp");
+    //fishes = _([a,b]).map((x) => x.name);
+    //return _(fishes).contains("Vip Viper");
     return false;
   }
   function compare(a, b) {
     return a < b ? -1 : b < a ? 1 : 0;
+  }
+  function winner(a, b, result) {
+    return result == -1 ? a.name : result == 1 ? b.name : "*tie*";
   }
 
   var maxTime = 0x7FFFFFFFFFFF;
@@ -104,6 +107,97 @@ sortByWindowPeriods = function() {
     return compareWindows(getWindowStart(a.catchableRanges, 0),
                           getWindowStart(b.catchableRanges, 0),
                           baseTime);
+  }
+
+  function isFishUpNow(f, baseTime) {
+    return dateFns.isBefore(getWindowStart(f.catchableRanges, 0), baseTime);
+  }
+
+  function isUpVerySoon(f, baseTime) {
+    return eorzeaTime.toEarth(getWindowStart(f.catchableRanges, 0)) <
+      +dateFns.addMinutes(eorzeaTime.toEarth(baseTime), 15);
+  }
+
+  function sortByOverallRarity(a, b, baseTime, completionManager) {
+    var result = 0;
+    // PINNED FISH ALWAYS COME FIRST!!!
+    var pinnedA = completionManager.isFishPinned(a.id) ? -1 : 1;
+    var pinnedB = completionManager.isFishPinned(b.id) ? -1 : 1;
+    result = compare(pinnedA, pinnedB);
+    if (result != 0) {
+      return result;
+    }
+    // Fish which are ALWAYS up should come AFTER fish with limited uptime.
+    var limitedA = a.alwaysAvailable ? 1 : -1;
+    var limitedB = b.alwaysAvailable ? 1 : -1;
+    result = compare(limitedA, limitedB);
+    if (shouldLog(a, b))
+      console.log("Comparing all-day availability:", winner(a,b,result),
+        "\n", a.name, a.alwaysAvailable,
+        "\n", b.name, b.alwaysAvailable);
+    if (result != 0) {
+      return result;
+    }
+
+    var aRanges = a.catchableRanges;
+    var bRanges = b.catchableRanges;
+
+    // How long is it up over the next n windows, relative to the other fish!
+    aUptime = a.uptime();
+    bUptime = b.uptime();
+
+    result = compare(isFishUpNow(a, baseTime) ? -1 : 1,
+                     isFishUpNow(b, baseTime) ? -1 : 1);
+    if (shouldLog(a, b))
+      console.log("Comparing 'is up now':", winner(a,b,result),
+        "\n", a.name, isFishUpNow(a, baseTime),
+        "\n", b.name, isFishUpNow(b, baseTime));
+    if (result != 0) return result;
+
+    // If both fish are now up yet, is one of them going to be up soon?
+    if (!isFishUpNow(a, baseTime)) {
+      var aUpSoon = isUpVerySoon(a, baseTime);
+      var bUpSoon = isUpVerySoon(b, baseTime);
+      result = compare(aUpSoon ? -1 : 1,
+                       bUpSoon ? -1 : 1);
+      if (shouldLog(a, b))
+        console.log("Comparing 'is up very soon':", winner(a,b,result), +dateFns.addMinutes(baseTime, 15),
+          "\n", a.name, aUpSoon, getWindowStart(aRanges, 0),
+          "\n", b.name, bUpSoon, getWindowStart(bRanges, 0));
+      if (result != 0) return result;
+    }
+
+    // If both are in the same state, compare by rarity (shorter comes first)
+    result = compare(aUptime, bUptime);
+    if (shouldLog(a, b))
+      console.log("Comparing uptime:", winner(a,b,result),
+        "\n", a.name, aUptime,
+        "\n", b.name, bUptime);
+    if (result != 0) return result;
+
+    // If both are the same, the fish with the longer time till next window
+    // comes first.
+    result = compare(getWindowStart(bRanges, 1) || 0,
+                     getWindowStart(aRanges, 1) || 0);
+    if (shouldLog(a, b))
+      console.log("Comparing time till next window:", winner(a,b,result),
+        "\n", a.name, getWindowStart(aRanges, 1),
+        "\n", b.name, getWindowStart(bRanges, 1));
+    if (result == 0) {
+      // Or, which ever fish's window closes first.
+      result = compare(getWindowEnd(aRanges, 0),
+                       getWindowEnd(bRanges, 0));
+      if (shouldLog(a, b))
+        console.log("Comparing remaining window time:", winner(a,b,result),
+          "\n", a.name, getWindowEnd(aRanges, 0),
+          "\n", b.name, getWindowEnd(bRanges, 0));
+
+      if (result == 0) {
+        // Ok fine... SORT BY ID!
+        result = compare(a.id, b.id);
+      }
+    }
+    return result;
   }
 
   function sortByWindowPeriods(a, b, baseTime, completionManager) {
@@ -176,7 +270,10 @@ sortByWindowPeriods = function() {
     return result;
   }
 
-  return sortByWindowPeriods;
+  return {
+    sortByWindowPeriods: sortByWindowPeriods,
+    sortByOverallRarity: sortByOverallRarity
+  };
 }();
 
 class ViewModel {
@@ -191,6 +288,7 @@ class ViewModel {
     this.theFish = Fishes;
     this.showUpcomingWindowFromNow = false;
     this.subscriptions = [];
+    this.sortingType = "windowPeriods";
 
     this.fishEntryTemplate = () => "";
   }
@@ -222,11 +320,23 @@ class ViewModel {
       })
       .value();
 
+    // Pick sorter.
+    var sorter = null;
+    console.log("Sorting fish by:", this.sortingType);
+    if (this.sortingType == 'windowPeriods') {
+      sorter = sorters.sortByWindowPeriods;
+    } else if (this.sortingType == 'overallRarity') {
+      sorter = sorters.sortByOverallRarity;
+    } else {
+      console.warn("Invalid sorting type:", this.sortingType);
+      sorter = sorters.sortByWindowPeriods;
+    }
 
     // Now, we can add the pinned fish back into the list before sorting
     // the fish by rarity.
     fishes = pinnedFish.concat(fishes).sort(
-      (a, b) => sortByWindowPeriods(a, b, baseTime, this.completionManager));
+      //(a, b) => sorters.sortByWindowPeriods(a, b, baseTime, this.completionManager));
+      (a, b) => sorter(a, b, baseTime, this.completionManager));
 
     return {
       fishes: fishes,
@@ -250,6 +360,18 @@ class ViewModel {
         caught: this.completionManager.isFishCaught(x.id),
         pinned: this.completionManager.isFishPinned(x.id),
         timerState: () => { return x.isCatchable() ? 'fish-active' : '' },
+        nextAvailBin: () => {
+          var crs = x.catchableRanges;
+          if (crs.length > 0) {
+            if (dateFns.isFuture(eorzeaTime.toEarth(+crs[0].start()))) {
+              var minutesUntilUp = dateFns.differenceInMinutes(eorzeaTime.toEarth(+crs[0].start()), Date.now());
+              if (minutesUntilUp < 15) {
+                return 'fish-bin-15';
+              }
+            }
+          }
+          return '';
+        },
         availability: {
           current: {
             duration: () => {
