95af71d25535a7fa3b99490f8c5ef8877bb96f03
icykoneko <icykoneko@users.noreply.github.com>
2019-05-06 20:47:43 -0400

    remove parts of master we don't need for deployments

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static(__dirname, {maxAge: 2592000000}))
-
-app.listen(3000, function() {
-  console.log('Listing on port 3000!')
-})
