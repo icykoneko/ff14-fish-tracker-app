6c63b89a3f2fd83adde25c2326bff97a65996349
icykoneko <icykoneko@users.noreply.github.com>
2019-08-26 17:58:56 -0400

    Fixes #40 by representing soft-hyphens with `&shy;`.

--- private/manageFishData.py
+++ private/manageFishData.py
@@ -87,7 +87,7 @@ def load_dats(args):
     _string_decoder.set_decoder(text.TagType.Emphasis.value, omit_tag_decoder)
     _string_decoder.set_decoder(
         text.TagType.SoftHyphen.value,
-        lambda i,t,l: text.nodes.StaticString(_string_decoder.dash))
+        lambda i,t,l: text.nodes.StaticString("\x26shy;"))
 
     return xiv
 
