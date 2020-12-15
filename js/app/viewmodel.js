6ad00e0eab1d145f55bc3f700ce4f9e3966cc7f9
icykoneko <icykoneko@users.noreply.github.com>
2020-12-14 21:56:31 -0500

    (closes #79) Periodically check for updates

--- js/app/viewmodel.js
+++ js/app/viewmodel.js
@@ -1184,4 +1184,51 @@ let ViewModel = new class {
     }
     return false;
   }
+
+  checkForUpdates(current_sha) {
+    this.currentVersionSha = current_sha;
+    console.log("Setting current deployment to as %s", current_sha);
+    // Obtain the last modified time information (this can be done asyncly)
+    octokit.repos.getDeployments({
+      owner: "icykoneko",
+      repo: "ff14-fish-tracker-app",
+      sha: current_sha
+    }).then(result => {
+      let siteUpdatedDate = new Date(Date.parse(result.data[0].updated_at));
+      $('#site-version').text(new Intl.DateTimeFormat(undefined, {
+        year: 'numeric', month: 'numeric', day: 'numeric',
+        hour: 'numeric', minute: 'numeric', second: 'numeric',
+        timeZoneName: 'short' }).format(siteUpdatedDate));
+    }).catch(error => { /* do nothing */ });
+    // Periodically check for updates by invoking onCheckForUpdates.
+    setTimeout(this.onCheckForUpdates.bind(this), 5 * 60 * 1000, current_sha);
+  }
+
+  onCheckForUpdates(current_sha) {
+    octokit.repos.getDeployments({
+      owner: "icykoneko",
+      repo: "ff14-fish-tracker-app",
+      per_page: 1
+    }).then(result => {
+      if (result.data.length > 0) {
+        console.log("Checking for updates... latest deployment is %s", result.data[0].sha);
+        if (result.data[0].sha != current_sha) {
+          // Change the last update menu item to reflect.
+          console.info("A newer version of the site is available. Please reload the page.");
+          $('#site-last-update')
+            .addClass('update-available')
+            .html("<i class=\"exclamation triangle icon\"></i> <span><a class=\"ui link\" href=\"\">Refresh</a> for Update!</span>");
+          $('#site-last-update a.link').on('click', function() {
+            // Reload the site.
+            document.refresh();
+          });
+        } else {
+          // Check again later.
+          setTimeout(this.onCheckForUpdates.bind(this), 5 * 60 * 1000, current_sha);
+        }
+      }
+    }).catch(error => {
+      console.warn("Failed to check for updates...", error);
+    });
+  }
 };
