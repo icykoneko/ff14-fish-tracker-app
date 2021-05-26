6d63ab8b49eade3b351d5ba12f8c79ea5374d6b7
icykoneko <icykoneko@users.noreply.github.com>
2021-05-26 00:11:44 -0400

    quiet noisy messages about localization

--- private/manageFishData.py
+++ private/manageFishData.py
@@ -133,7 +133,7 @@ def _make_localized_field(fld_name, row, col_name):
 
         # Use the default language name instead...
         value = row[col_name]
-        logging.warning("Missing %s data for %s[%u][%s], using \"%s\" instead.",
+        logging.debug("Missing %s data for %s[%u][%s], using \"%s\" instead.",
                         lang.name,
                         row.sheet.name,
                         row.key,
