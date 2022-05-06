bc61d919eedcff0bd5d9d970ab23fcdf46da7e3c
icykoneko <icykoneko@users.noreply.github.com>
2022-05-06 15:06:53 -0400

    Provide links to fish guides (#115)

---
+++ js/app/data_ex.js
@@ -0,0 +1,9 @@
+///////////////////////////////////////////////////////////////////////////////
+// Extra Data
+
+const EXTRA_DATA = {
+    FISH: {
+        // TODO: Get list from Fruity!
+        "Aetherolectric Guitarfish": { video: { youtube: "gZUhT4k0hb0" } },
+    },
+};
