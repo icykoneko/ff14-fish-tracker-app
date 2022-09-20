853492495f3b1f42763362e48fb2b70105d8911e
icykoneko <icykoneko@users.noreply.github.com>
2022-09-20 08:25:44 -0400

    Added link to Bigcuda video guide

--- js/app/data_ex.js
+++ js/app/data_ex.js
@@ -102,5 +102,6 @@ const EXTRA_DATA = {
         "Drepanaspis": { video: { youtube: "ObesgoS8szY" } },
         "Hippo Frog": { video: { youtube: "H4tjuH9cafA" } },
         "Mayashell": { video: { youtube: "9bqf7Qd_a-4" } },
+        "Bigcuda": { video: { youtube: "pi_7oLORVWo" } },
     },
 };
