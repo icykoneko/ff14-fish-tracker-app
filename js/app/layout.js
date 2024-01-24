2919b88286c523ef7c3883402ffa906f27d1b7a8
C. Bland <bland.cr@gmail.com>
2024-01-24 10:11:50 -0800

    (fixes #150) detect inituition fish availability changes
    When the layout updates subEntries (intuition fish) it does not capture
    whether the subEntry availability has changed. This means intuition fish
    availability changes don't provoke a layout sort. Without a layout sort
    the "fish-availability-transitioning" class won't get removed, so the
    text for the time remaining remains set of 0 opacity.

--- js/app/layout.js
+++ js/app/layout.js
@@ -215,8 +215,10 @@ class FishTableLayout {
 
     // Update any intuition fish rows as well!
     for (let subEntry of fishEntry.intuitionEntries) {
-      if (!subEntry.data.alwaysAvailable)
-        this.update(subEntry, baseTime, needsFullUpdate);
+      if (!subEntry.data.alwaysAvailable) {
+        let changed = this.update(subEntry, baseTime, needsFullUpdate);
+        hasFishAvailabilityChanged ||= changed;
+      }
     }
 
     // Let the caller know this fish changed availability or bins.
